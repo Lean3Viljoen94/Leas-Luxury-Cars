455944aeab95b41eb8cfd8bbc5776a355c2e1ee3
Leandri <lean331@gmail.com>
2019-11-26 15:31:15 +0200

    adding src files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CarCards from './Components/CarCards';
+import CarCarousel from './Components/Carousel';
+import CarHeader from './Components/Header';
+import Filters from './Components/Filters';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CarHeader/>
+      <CarCarousel/>
+      <br/>
+      <Filters/>
+      <CarCards/>
     </div>
   );
 }
